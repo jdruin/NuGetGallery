40a78fac4a23ea02cfd4ff978e87f6b4d7c28a95
Jeffrey Druin <jdruin1@gmail.com>
2019-09-25 15:37:17 -0500

    Added framework for interal AD auth

---
+++ src/NuGetGallery.Services/Authentication/Providers/ActiveDirectory/ActiveDirectoryAuthenticator.cs
@@ -0,0 +1,13 @@
+﻿using NuGetGallery.Authentication.Providers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuGetGallery.Services.Authentication.Providers.ActiveDirectory
+{
+    public class ActiveDirectoryAuthenticator : Authenticator<ActiveDirectoryAuthenticatorConfiguration>
+    {
+    }
+}
